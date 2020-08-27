03b72c044bc49ce22fe1e6aacb6929326c128a57
m-kitano <ktn.msm53@gmail.com>
2020-08-27 13:54:40 +0000

    add category post comment validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
   
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :title, presence: true
+  validates :content, presence: true
   
 end
